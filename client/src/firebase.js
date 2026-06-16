8cc83204cff05797d86fc1d277b83502bd553921
YASH NEWRISE <yashu3801@gmail.com>
2026-06-16 22:49:50 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFMhsbxIs85Qc_UifxcNcvmQfGEOPcJjo",
-  authDomain: "auros-fc5b8.firebaseapp.com",
-  projectId: "auros-fc5b8",
-  storageBucket: "auros-fc5b8.firebasestorage.app",
-  messagingSenderId: "966257035883",
-  appId: "1:966257035883:web:5cd1902f2cea459da2ff2e",
-  measurementId: "G-DF723FGPRR"
+  apiKey:,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
 };
 
 const app = initializeApp(firebaseConfig);
